47d2b008c20c2c1c1c79b895e887f237598deac7
yamaniyuda <yamaniyuda@gmail.com>
2025-11-03 14:28:35 +0000

    fix: merge

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> bb19531 (fix: merge)
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
+<<<<<<< HEAD
   Future<Map<String, dynamic>?> getWeather(
       {double lat = 3.5952, double lon = 98.6722}) async {
         
@@ -30,4 +35,16 @@ class WeatherService {
     'medan': {'lat': 3.5952, 'lon': 98.6722},
     'jakarta': {'lat': -6.2000, 'lon': 106.8166},
   };
+=======
+  Future<Map<String, dynamic>> getWeather() async {
+    // Replace with a real API call
+    await Future.delayed(const Duration(seconds: 1));
+    return {
+      "weather": [
+        {"main": "Clear", "icon": "01d"}
+      ],
+      "main": {"temp": 25.0}
+    };
+  }
+>>>>>>> bb19531 (fix: merge)
 }
